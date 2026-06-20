63421c90fb1c8a483937000e5471e20272906b8c
Arinjay_Shirwadkar <arinjayx@gmail.com>
2026-06-20 10:19:32 +0530

    Problems in part 5

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def longest(strs):
+    l=0
+    for s in strs:
+        if len(s)>l:
+            l=len(s)
+            longest=s
+    return longest
+
+if __name__=="__main__":
+    print(longest(["Hello", "World", "Stupide editor"]))
