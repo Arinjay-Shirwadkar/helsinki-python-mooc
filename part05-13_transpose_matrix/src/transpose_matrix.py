3887cc3c397995a3d7ac7225ca9fa591b65fca60
Arinjay_Shirwadkar <arinjayx@gmail.com>
2026-06-21 13:59:46 +0530

    Problems in part 5

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,8 @@
-# Write your solution here
+def transpose(mat: list):
+
+    for i in range(0,len(mat)):
+        for j in range(0,len(mat[i])):
+            if j>=i:
+                c=mat[i][j]
+                mat[i][j]=mat[j][i]
+                mat[j][i]=c
