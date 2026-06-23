b096feea0e8ecd0933cbcb4a430b07a92745a89f
Arinjay_Shirwadkar <arinjayx@gmail.com>
2026-06-23 15:54:33 +0530

    Problems in part 5

--- part05-20_numbers_spelled_out/src/numbers_spelled_out.py
+++ part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -1,24 +1,34 @@
 def dict_of_numbers():
-    dic={}
-    units={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve'}
-    tens={2:"twen",3:"thir",4:"four",5:"fif",6:"six",7:"seven",8:"eigh",9:"nine"}
+    dic = {}
     
-    for i in range(0,100):
-        if i<=12:
-         dic[i]=units[i]
-        elif i<20:
-           u=i%10
-           s1=tens[u]
-           dic[i]=s1+'teen'
+    # Numbers 0 through 19 are unique and best mapped explicitly
+    words_0_to_19 = {
+        0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+        6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+        11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+        15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'
+    }
+    
+    # Tens prefixes strictly for numbers >= 20
+    tens_prefixes = {
+        2: "twenty", 3: "thirty", 4: "forty", 5: "fifty",
+        6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"
+    }
+    
+    for i in range(0, 100):
+        if i < 20:
+            dic[i] = words_0_to_19[i]
         else:
-           t=i//10
-           u=i%10
-           s1=tens[t]
-           if u!=0:
-              s2=units[u]
-              dic[i]=s1+'ty-'+s2
-           else:
-              dic[i]=s1+'ty'
+            t = i // 10
+            u = i % 10
+            prefix = tens_prefixes[t]
+            
+            if u != 0:
+                dic[i] = prefix + '-' + words_0_to_19[u]
+            else:
+                dic[i] = prefix
+                
     return dic
-if __name__=="__main__":
-   print(dict_of_numbers())
+
+if __name__ == "__main__":
+    print(dict_of_numbers())
