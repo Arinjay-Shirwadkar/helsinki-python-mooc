ae3b478fbfbddb0bef7e55fcb888abb4aafeb6c6
Arinjay_Shirwadkar <arinjayx@gmail.com>
2026-06-28 07:03:48 +0530

    Problems in part 6

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def read_input(s,l,u):
+    while True:
+        try:
+            n = int(input(s))
+            if n>=l and n<=u:
+                return n
+        except ValueError:
+            pass
+        
+        print(f"You must type in an integer between {l} and {u}")
+
+#number = read_input("Please type in a number: ", 5, 10)
+#print("You typed in:", number)
+
--- part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,25 @@
-# Write your solution here
+#1 and 39 inclusive.
+def filter_incorrect():
+
+    with open("lottery_numbers.csv") as file:
+        with open('correct_numbers.csv','w') as file2:
+            for line in file:
+                hash =[0]*40
+                temp = line
+                line = line[5:]
+                line = line.strip()
+                ind = line.find(';')
+                week_no = line[:ind]
+                line = line[ind+1:]
+                nums = line.split(',')
+                if len(nums)!=7 or (not week_no.isnumeric()):
+                    continue
+                flag=1
+                for num in nums:
+                    if not num.isnumeric() or int(num)<1 or int(num)>39 or hash[int(num)]!=0:
+                        flag=0
+                        break
+                    hash[int(num)]+=1
+                if flag==1:
+                    file2.write(temp)
+    
